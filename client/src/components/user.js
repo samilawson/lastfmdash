932a24cffada8700493d8f3e770b2ed2bbac39cc
Sam Lawson <samilawson17@gmail.com>
2019-02-09 15:24:43 -0600

    fixed user component

--- client/src/components/user.js
+++ client/src/components/user.js
@@ -1,13 +1,27 @@
 import React from "react";
 import axios from "axios";
 import { constants } from "../constants/constants";
+import styled from "styled-components";
 const { baseURL, apiKey, userName } = constants;
+
+const UserLink = styled.a`
+  display: inline-block;
+  margin: 5px auto;
+  background-color: rgba(255, 255, 255, 1);
+`;
+
+const UserInfo = styled.ul`
+  margin: 0 auto;
+  padding: 1rem 0.7rem 0.7rem 0;
+  text-align: right;
+  color: darkGrey;
+`;
 
 class User extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: []
+      user: null
     };
   }
   componentDidMount() {
@@ -18,16 +32,30 @@ class User extends React.Component {
     getUserInfo().then(response => {
       let data = response.data;
       console.log(data.user.playcount); //logs second, displays correct
-      this.setState(state => ({
-        user: data
-      }));
+      this.setState({
+        user: data.user
+      });
     });
   }
 
   render() {
     console.log(this.state); //logs first and third, doesn't work of course on first but does on third
-   return (<p>{this.state.user.length ? this.state.user.user.playcount : 'loading'}</p>)
-
+    if(this.state.user){
+      return (<UserLink
+      href={this.state.user.url} target="_blank">
+      <div>
+        <img
+          src={this.state.user.image['3']['#text']}
+          alt={this.state.user.name} />
+      </div>
+      <UserInfo>
+        <li>{this.state.user.name}</li>
+        <li>{this.state.user.playcount}</li>
+      </UserInfo>
+      </UserLink> )
+    } else {
+      return <p>No data</p>
+    }
    
   }
 }
