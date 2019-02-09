7772ffb5d5f9dbeeedc88bfb1f95928b71841af3
Sam Lawson <samilawson17@gmail.com>
2019-02-09 15:35:05 -0600

    improved navbar

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,25 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-
 import { logout } from "../store/actions";
-
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 const NavBar = ({ auth, logout }) => (
   <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/register">Register</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
-      <li>
-        <Link to="/test">Test</Link>
-      </li>
-    </ul>
+    <Navbar color="dark" light expand="md">
+      <NavbarBrand href="/">lastfmdash</NavbarBrand>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/register">Register</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="/login">Login</NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+
     {auth.isAuthenticated && <p>Logged in as {auth.user.username}</p>}
   </div>
 );
